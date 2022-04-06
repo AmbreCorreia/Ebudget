5a5e9a4061e500218bb61400618b8d99790f1a10
ThomasTricot <thomas.tricot@etu.unice.fr>
2022-04-06 15:05:28 +0200

    Principaux layout

---
+++ app/src/main/java/edu/polytech/ebudget/Home.java
@@ -0,0 +1,14 @@
+package edu.polytech.ebudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/edu/polytech/ebudget/ListeDeCourse.java
@@ -0,0 +1,14 @@
+package edu.polytech.ebudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListeDeCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_de_course);
+    }
+}
