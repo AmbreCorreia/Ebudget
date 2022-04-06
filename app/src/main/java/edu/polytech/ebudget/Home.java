d7f761c96a81b51b8ca5e2f7b0f49827b92db97a
AmbreCorreia <74357454+AmbreCorreia@users.noreply.github.com>
2022-04-06 17:06:41 +0200

    circle progress bar

--- app/src/main/java/edu/polytech/ebudget/Home.java
+++ app/src/main/java/edu/polytech/ebudget/Home.java
@@ -3,12 +3,54 @@ package edu.polytech.ebudget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+
+    private int progress = 0;
+    private Button increment;
+    private Button decrement;
+    private TextView textviewprogress;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        increment = findViewById(R.id.button_incr);
+        decrement = findViewById(R.id.button_decr);
+        textviewprogress = findViewById(R.id.text_view_progress);
+        progressBar = findViewById(R.id.progress_bar);
+
+        updateProgressBar();
+
+        increment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (progress <= 90) {
+                    progress += 10;
+                    updateProgressBar();
+                }
+            }
+        });
+
+        decrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (progress >= 10) {
+                    progress -= 10;
+                    updateProgressBar();
+                }
+            }
+        });
+    }
+
+    private void updateProgressBar() {
+        progressBar.setProgress(progress);
+       textviewprogress.setText(progress+"%");
     }
 }
