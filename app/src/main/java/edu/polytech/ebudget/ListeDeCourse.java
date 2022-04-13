ac711d6c6c1e34cfac8f5d51521197bfbba30a13
ThomasTricot <thomas.tricot@etu.unice.fr>
2022-04-13 17:46:32 +0200

    ajout du bottom menu

--- app/src/main/java/edu/polytech/ebudget/ListeDeCourse.java
+++ app/src/main/java/edu/polytech/ebudget/ListeDeCourse.java
@@ -2,7 +2,10 @@ package edu.polytech.ebudget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListeDeCourse extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class ListeDeCourse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_de_course);
+
+        Button btn = (Button) findViewById(R.id.returnButton5);
+        btn.setOnClickListener(this::returnButton);
+    }
+
+    private void returnButton(View v){
+        Intent intent = new Intent(ListeDeCourse.this, MainActivity.class);
+        startActivity(intent);
     }
 }
