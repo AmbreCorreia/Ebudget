aaa0a95d579e191f0651103bd6eb2ba4d87c0b09
ThomasTricot <thomas.tricot@etu.unice.fr>
2022-04-20 16:20:08 +0200

    ajout du bouton home

--- app/src/main/java/edu/polytech/ebudget/MainActivity.java
+++ app/src/main/java/edu/polytech/ebudget/MainActivity.java
@@ -6,11 +6,14 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import edu.polytech.ebudget.databinding.ActivityMainBinding;
 import edu.polytech.ebudget.fragmentsFooter.FragmentCourses;
 import edu.polytech.ebudget.fragmentsFooter.FragmentCategory;
+import edu.polytech.ebudget.fragmentsFooter.FragmentHome;
 import edu.polytech.ebudget.fragmentsFooter.FragmentNotif;
 import edu.polytech.ebudget.fragmentsFooter.FragmentProfil;
 import edu.polytech.ebudget.fragmentsFooter.FragmentRetour;
@@ -26,7 +29,6 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide();
 
         replaceFragment(new FragmentRetour());
 
@@ -60,5 +62,24 @@ public class MainActivity extends AppCompatActivity{
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // R.menu.mymenu is a reference to an xml file named mymenu.xml which should be inside your res/menu directory.
+        // If you don't have res/menu, just create a directory named "menu" inside res
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.home0) {
+            replaceFragment(new FragmentHome());
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
