e7d9c0ee514fb635d887880e749a6afd0442bed0
AmbreCorreia <74357454+AmbreCorreia@users.noreply.github.com>
2022-04-23 18:09:41 +0200

    ne crash pas dans l'emulateur

--- app/src/main/java/edu/polytech/ebudget/utils/CalendarHelper.java
+++ app/src/main/java/edu/polytech/ebudget/utils/CalendarHelper.java
@@ -17,41 +17,53 @@ public class CalendarHelper {
 
     public CalendarHelper(){}
 
-    public static void addCalendarEvent(Activity activity, Context context, String msg){
-        String calendarEmailAddress= FirebaseAuth.getInstance().getCurrentUser().getEmail();
-        Log.d("agenda email", calendarEmailAddress);
+    public static void addCalendarEvent(Activity activity, Context context, String msg) {
+        String calendarEmailAddress = FirebaseAuth.getInstance().getCurrentUser().getEmail();
 
-        long calendarID = 0;
+        long calendarID = -1;
 
         String[] projection = new String[]{
                 CalendarContract.Calendars._ID,
                 CalendarContract.Calendars.ACCOUNT_NAME};
 
-        ContentResolver cr = activity.getContentResolver();
+       try {
+            ContentResolver cr = activity.getContentResolver();
+            Cursor cursor = cr.query(CalendarContract.Calendars.CONTENT_URI, projection,
+                    CalendarContract.Calendars.ACCOUNT_NAME + "=? and (" +
+                            CalendarContract.Calendars.NAME + "=? or " +
+                            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME + "=?)",
+                    new String[]{calendarEmailAddress, calendarEmailAddress,
+                            calendarEmailAddress}, null);
 
-        Cursor cursor = cr.query(CalendarContract.Calendars.CONTENT_URI, projection,
-                CalendarContract.Calendars.ACCOUNT_NAME + "=? and (" +
-                        CalendarContract.Calendars.NAME + "=? or " +
-                        CalendarContract.Calendars.CALENDAR_DISPLAY_NAME + "=?)",
-                new String[]{calendarEmailAddress, calendarEmailAddress,
-                        calendarEmailAddress}, null);
-
-        if (cursor.moveToFirst()) {
-            if (cursor.getString(1).equals(calendarEmailAddress)){
-                calendarID = cursor.getInt(0);
+            if (cursor.moveToFirst()) {
+                if (cursor.getString(1).equals(calendarEmailAddress)) {
+                    calendarID = cursor.getInt(0);
+                }
             }
-        }
 
-        ContentValues values = new ContentValues();
-        values.put(CalendarContract.Events.DTSTART, android.icu.util.Calendar.getInstance().getTimeInMillis());
-        values.put(CalendarContract.Events.DTEND, android.icu.util.Calendar.getInstance().getTimeInMillis()+1);
-        values.put(CalendarContract.Events.TITLE, "Ebudget");
-        values.put(CalendarContract.Events.DESCRIPTION, msg);
-        values.put(CalendarContract.Events.CALENDAR_ID, calendarID);
-        values.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().getID());
-        Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+            if (calendarID != -1) {
 
-        Toast.makeText(context, "Event pushed to Calendar",
-                Toast.LENGTH_SHORT).show();
+                ContentValues values = new ContentValues();
+                values.put(CalendarContract.Events.DTSTART, android.icu.util.Calendar.getInstance().getTimeInMillis());
+                values.put(CalendarContract.Events.DTEND, android.icu.util.Calendar.getInstance().getTimeInMillis() + 1);
+                values.put(CalendarContract.Events.TITLE, "Ebudget");
+                values.put(CalendarContract.Events.DESCRIPTION, msg);
+                values.put(CalendarContract.Events.CALENDAR_ID, calendarID);
+                values.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().getID());
+                Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+
+                Toast.makeText(context, "Event pushed to Calendar",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "No calendar found",
+                        Toast.LENGTH_SHORT).show();
+            }
+       } catch (SecurityException e){
+           activity.runOnUiThread(() -> Toast.makeText(context, "Failure: Permission denied",
+                   Toast.LENGTH_SHORT).show());
+       } catch (Exception exception) {
+           activity.runOnUiThread(() -> Toast.makeText(context, "Failure: unknown error",
+                   Toast.LENGTH_SHORT).show());
+       }
     }
 }
